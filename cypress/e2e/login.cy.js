477dfbf11565d815ea7b0f71954e2450cf0a8a8f
Rafael Campos <rafaelcampos@Rafaels-MacBook-Pro.local>
2025-03-20 20:52:44 -0300

    Configure cypress grep3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,12 @@
 
 describe('Login', () => {
-    it('successfully logs in', () => {
-      cy.intercept('GET', '**/notes').as('getNotes') // ao executar o login ele carrega notes (olhando o no intercep do network no navegador)
-     cy.guiLogin() // aqui ele chama o login do commands.js
-      cy.wait('@getNotes') // por isso aqui aguarda a resposta do getNotes para depois continuar e verificar a primeira pagina
-  
-      
-      cy.contains('a', 'Create a new note').should('be.visible')
-    })
+  it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes') // ao executar o login ele carrega notes (olhando o no intercep do network no navegador)
+    cy.guiLogin() // aqui ele chama o login do commands.js
+    cy.wait('@getNotes') // por isso aqui aguarda a resposta do getNotes para depois continuar e verificar a primeira pagina
+
+
+    cy.contains('a', 'Create a new note').should('be.visible')
   })
-  
+})
+
