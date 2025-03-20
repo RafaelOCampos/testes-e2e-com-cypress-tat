46eecbf12c88217e493fbaee4180378704bcb0a7
Rafael Campos <rafaelcampos@Rafaels-MacBook-Pro.local>
2025-03-20 20:43:31 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,11 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768, //ate 768px é web, abaixo é mobile
     },
-    defaultCommandTimeout: 50000, // comando padrão de tempo limite para não tomar timeout padrão 4000ms
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/grep/src/plugin')(config)
+      return config
     },
+    defaultCommandTimeout: 50000, // comando padrão de tempo limite para não tomar timeout padrão 4000ms
   },
+  projectId: 'your-project',
 })
